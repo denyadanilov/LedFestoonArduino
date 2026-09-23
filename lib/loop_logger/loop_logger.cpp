86849dbd350cce2c52f6a922fd2bc5e409e7005e
Denys Danylov <d2b2ukr.net>
2026-09-23 10:58:08 +0300

    Structure of the logger was updated to improve readability

--- lib/loop_logger/loop_logger.cpp
+++ lib/loop_logger/loop_logger.cpp
@@ -8,28 +8,34 @@ unsigned long loop_iteration_num_ = 0;
 unsigned long total_iteration_time_ = 0;
 char log_buffer_[32] = {0};
 
-bool try_log_loop_time(unsigned long time);
+bool try_log_loop_time();
+void print_loop_time(unsigned long average_iteration_time);
+void update_loop_stats(unsigned long micros);
 
 void log_loop_time(void (*command)()) {
   auto time = get_micros_from_start();
   command();
-  try_log_loop_time(time);
+  update_loop_stats(time);
+  try_log_loop_time();
 }
 
-void print_loop_time() {
-  auto average_iteration_time = total_iteration_time_ / loop_iteration_num_;
+void print_loop_time(unsigned long average_iteration_time) {
   snprintf(log_buffer_, sizeof(log_buffer_), "Average iteration time: %lu",
            average_iteration_time);
   log_message(log_buffer_);
 }
 
-bool try_log_loop_time(unsigned long micros) {
-  auto iteration_time = get_micros_from_start() - micros;
-  loop_iteration_num_++;
-  total_iteration_time_ += iteration_time;
+bool try_log_loop_time() {
   if (loop_iteration_num_ % LOG_ITERATIONS_NUM == 0) {
-    print_loop_time();
+    auto average_iteration_time = total_iteration_time_ / loop_iteration_num_;
+    print_loop_time(average_iteration_time);
     return true;
   }
   return false;
+}
+
+void update_loop_stats(unsigned long micros) {
+  auto iteration_time = get_micros_from_start() - micros;
+  loop_iteration_num_++;
+  total_iteration_time_ += iteration_time;
 }
